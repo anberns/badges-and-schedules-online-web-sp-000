ebfe75d5dd1edb8a64165b37bd23bc7d50374f96
Aaron Berns <anberns@gmail.com>
2019-01-07 19:50:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,13 +3,11 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
   
-
-  
-  #batch_badge_creator
-  def batch_badge_creator(names)
-    messages = []
-    names.each do |name|
-      messages << badge_maker(name)
-    end
-    messages
+#batch_badge_creator
+def batch_badge_creator(names)
+   messages = []
+  names.each do |name|
+    messages << badge_maker(name)
   end
+  messages
+end
