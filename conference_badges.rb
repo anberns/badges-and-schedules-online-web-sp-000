46316cc5ffa798e987f39ecf517342c3af52f190
Aaron Berns <anberns@gmail.com>
2019-01-07 19:45:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+You're hosting a conference and need to print badges for the speakers. Each badge should read: "Hello, my name is _____." Write a `badge_maker` method that, when provided a person's name, will create and return this message. E.g.:
+
+```bash
+badge_maker("Arel")
+=> "Hello, my name is Arel."
+# badge_maker
+def badge_maker(names)
+  names.each 
